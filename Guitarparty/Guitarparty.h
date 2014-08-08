7f83686e3863f48af149d0616eebc5717fa4d830
Matthew Wyskiel <mwyskiel@optonline.net>
2014-08-08 00:13:07 -0400

    Initial Commit

---
+++ Guitarparty/Guitarparty.h
@@ -0,0 +1,19 @@
+//
+//  Guitarparty.h
+//  Guitarparty
+//
+//  Created by Matthew Wyskiel on 8/8/14.
+//  Copyright (c) 2014 Guitarparty.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Guitarparty.
+FOUNDATION_EXPORT double GuitarpartyVersionNumber;
+
+//! Project version string for Guitarparty.
+FOUNDATION_EXPORT const unsigned char GuitarpartyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Guitarparty/PublicHeader.h>
+
+
